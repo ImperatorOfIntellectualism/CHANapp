6f1485db2369ab6b051f44718decf53ed0434b3b
King of Kings <Bachuganchikomania2@gmail.com>
2021-04-26 15:20:24 +0700

    Removed comment

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3000, //process.env возвращает все возможные данные о системе пользователя
-  MONGO_URL: "mongodb://localhost:27017/shop", //Адрес БД
-  EMAIL: "Bachuganchikomania2@gmail.com" //Email адрес
-};
